8f393bc9f746837082db00b344b49cc153fe1d5c
신희진 <gmlwls2407@gmail.com>
2025-04-16 00:00:42 +0900

    fix: basePath 적용 후 재배포

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,7 @@ try {
 
 const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   output: 'export',
   basePath: isProd ? '/SINIJINI' : '',
